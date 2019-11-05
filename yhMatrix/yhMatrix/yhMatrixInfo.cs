e5f15debea951160cf1d291f05a588d8a13fba56
Robin HE <54180511+Robin-HE@users.noreply.github.com>
2019-11-05 20:58:57 +0000

    Add files via upload

---
+++ yhMatrix/yhMatrix/yhMatrixInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace yhMatrix
+{
+    public class yhMatrixInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "yhMatrix";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("96e00b86-e577-4661-93a8-6cf43581c97a");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
